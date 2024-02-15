a005b4afcac3e3a9dd9c148aa488f722cadcb9b3
mhernandezhimalaya <158264106+mhernandezhimalaya@users.noreply.github.com>
2024-02-15 10:44:45 +0100

    Delete descargarPgAdmin4.sh

--- descargarPgAdmin4.sh
+++
@@ -1,4 +0,0 @@
-sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
-echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" | sudo tee /etc/apt/sources.list.d/pgadmin4.list
-sudo apt update
-sudo apt install pgadmin4
